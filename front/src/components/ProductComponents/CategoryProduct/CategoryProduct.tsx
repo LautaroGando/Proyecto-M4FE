9095053007c186f7a5e07825c11f20be8c7dedf2
Lautaro Gando <lautarogandodev@gmail.com>
2024-10-01 18:36:36 -0300

    Proyecto Finalizado

--- front/src/components/ProductComponents/CategoryProduct/CategoryProduct.tsx
+++ front/src/components/ProductComponents/CategoryProduct/CategoryProduct.tsx
@@ -23,7 +23,7 @@ const CategoryProduct: React.FC = (): React.ReactElement | null => {
 
     };
 
-    return <h1 className='textStrokeWhite text-5xl text-transparent font-bold capitalize self-start sm:text-6xl'>{category}</h1>
+    return <h1 className='text-5xl text-[#f3f4f6] font-bold capitalize self-start sm:text-6xl'>{category}</h1>
 
 };
 
