f67c14f87f3979f9b6a182d16d250888a87fc3bd
Lautaro Gando <lautarogandodev@gmail.com>
2024-10-05 00:40:18 -0300

    Proyecto Finalizado

---
+++ front/next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://proyecto-m4-fe.vercel.app/',
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+    changefreq: 'daily',
+    priority: 0.7,
+};
